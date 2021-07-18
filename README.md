634ebefa1db64d0c8e792d978c0374910f28d724
user <huryn.victar@gmail.com>
2021-07-18 20:22:18 +0300

    plain changelog

---
+++ README.md
@@ -0,0 +1,17 @@
+# Changelog docker action
+
+This action create output from all PR (title and url) that was merged since previous tag.
+
+## Inputs
+
+### No inputs required
+
+## Outputs
+
+## `changelog`
+
+multistring result
+
+## Example usage
+
+uses: actions/changelog@v1
---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+time=$(git log -1 --date=short --format=%ai $(git log --oneline $(git show-ref --tags |tail -2 |head -1|awk '{print $1}')..@|tail -1|awk '{print $1}')|sed 's/ /T/g' )
+changelog=$(gh pr list -B main -s closed --json 'title,createdAt,url'  -q ".[]|select(.createdAt >= \"$time\")" | jq -r '[ .title, .url]|@tsv')
+
+changelog="${changelog//'%'/'%25'}"
+changelog="${changelog//$'\n'/'%0A'}"
+changelog="${changelog//$'\r'/'%0D'}"
+echo "::set-output name=changelog::$changelog"
