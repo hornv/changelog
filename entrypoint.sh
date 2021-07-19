5fb428092cbc6a98e7adb1fd9a017bf5d605a890
user <huryn.victar@gmail.com>
2021-07-19 20:52:53 +0300

    test docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-whoami
-ls -la /
-ls -la /github
-time=$(git log -1 --date=short --format=%ai $(git log --oneline $(git show-ref --tags |tail -2 |head -1|awk '{print $1}')..@|tail -1|awk '{print $1}')|sed 's/ /T/g' )
-changelog=$(gh pr list -B main -s closed --json 'title,createdAt,url'  -q ".[]|select(.createdAt >= \"$time\")" | jq -r '[ .title, .url]|@tsv')
+#whoami
+#ls -la /
+#ls -la /github
+#time=$(git log -1 --date=short --format=%ai $(git log --oneline $(git show-ref --tags |tail -2 |head -1|awk '{print $1}')..@|tail -1|awk '{print $1}')|sed 's/ /T/g' )
+
+# time=$(git log -1 --date=short --format=%ai $(git log --oneline $(git show-ref --tags |tail -2 |head -1|awk '{print $1}')..@|tail -1|awk '{print $1}')|sed 's/ /T/g' )
+
+# changelog=$(gh pr list -B main -s closed --json 'title,createdAt,url'  -q ".[]|select(.createdAt >= \"$time\")" | jq -r '[ .title, .url]|@tsv')
 
 changelog="${changelog//'%'/'%25'}"
 changelog="${changelog//$'\n'/'%0A'}"
