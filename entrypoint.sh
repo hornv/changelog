0250a0edac3a646522d66241854c06b5d2121b86
user <huryn.victar@gmail.com>
2021-07-19 23:30:32 +0300

    test docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@
 #ls -la /github
 #time=$(git log -1 --date=short --format=%ai $(git log --oneline $(git show-ref --tags |tail -2 |head -1|awk '{print $1}')..@|tail -1|awk '{print $1}')|sed 's/ /T/g' )
 
+cd /github/workspace
 time=$(git log -1 --date=short --format=%ai $(git log --oneline $(git show-ref --tags |tail -2 |head -1|awk '{print $1}')..@|tail -1|awk '{print $1}')|sed 's/ /T/g' )
 
 changelog=$(gh pr list -B main -s closed --json 'title,createdAt,url'  -q ".[]|select(.createdAt >= \"$time\")" | jq -r '[ .title, .url]|@tsv')
